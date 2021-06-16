603e228f49a5dc5a2562787aa16cdd0b74f095ef
Mehdi Benrefad <benrefadmehdi@gmail.com>
2021-06-16 19:07:21 +0100

    Initial Commit

---
+++ FI-GI-IOS-APP-BY-MEHDI-BENREFAD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FI-GI-IOS-APP-BY-MEHDI-BENREFAD
+//
+//  Created by Mehdi Benrefad on 16/06/2021.
+//  Copyright © 2021 Mehdi Benrefad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
